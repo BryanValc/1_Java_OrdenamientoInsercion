e2a4329631117561a89212ea2004a34c6ea97365
Bryan Valdez <bryan501th@gmail.com>
2020-11-21 16:44:08 -0600

    Se creó la clase Ordenador, clase que contiene el método ordenarInsercion, método que recibe como parámetro un arreglo de enteros y los ordena

--- src/PruebaOrdenamientoInsercion.java
+++ src/PruebaOrdenamientoInsercion.java
@@ -1,8 +1,31 @@
 
+class Ordenador{
+	
+	public void ordenarInsercion(int[]numeros) {
+		int aux;
+		
+		for (int i = 1; i < numeros.length; i++) {
+			aux=numeros[i];
+			
+			for (int j=i-1; j>=0&&numeros[j]>aux ; j--) {
+				
+				numeros[j+1]=numeros[j];
+				numeros[j]=aux;
+				
+			}//segundo for
+			
+		}//primer for
+		
+	}//metodo ordenarInsercion
+	
+}
+
 public class PruebaOrdenamientoInsercion {
 
 	public static void main(String[] args) {
-
+		
+		
+		
 	}
 
 }
